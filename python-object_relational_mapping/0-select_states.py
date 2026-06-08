63d7475a43f21443764617e18eabcdae6d0788b1
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-08 16:44:36 +0200

    Add scripts for state listing and JSON serialization/deserialization

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module to list all states from database"""
+
+import MySQLdb
+import sys
+#!/usr/bin/python3
+"""Module to list all states from database"""
+
+import MySQLdb
+import sys
+
+def list_states():
+    """Lists all states sorted by id"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    
+    for row in results:
+        print(row)
+    
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_states()
