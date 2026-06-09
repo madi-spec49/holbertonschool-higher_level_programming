fb744819e0b951fcab24a2ad1c36b0f200f00583
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-09 11:13:53 +0200

    filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC",
         (sys.argv[4],)
     )
     for row in cur.fetchall():
