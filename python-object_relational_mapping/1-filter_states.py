bf51db8c9e3ecd9a0082125de1f82c26008cbae6
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-09 10:00:36 +0200

    filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cursor.fetchall()
 
     for state in states:
