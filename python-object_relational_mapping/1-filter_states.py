bae61d5ccdc84069cd879f6f3d80a62ce26c81b3
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-09 09:53:16 +0200

    filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-"""Module to list all states from database"""
+"""Module to list states starting with N from database"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE'N%' ORDER BY id ASC")
     states = cursor.fetchall()
+
     for state in states:
         print(state)
+
     cursor.close()
     db.close()
