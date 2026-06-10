a3f3b507404ec922fbde873c88236e9636f5e825
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-10 21:43:42 +0200

    task14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """City class inherits from Base and links to the MySQL table cities"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
