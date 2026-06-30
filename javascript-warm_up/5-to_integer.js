1678fd8be943c72f499b5b6ca0072a5fd57e6df8
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-30 11:11:14 +0200

    Add 5-to_integer.js to convert command line argument to an integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg = process.argv[2];
 const num = Number(arg);
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number: " + parseInt(arg));
+  console.log('My number: ' + parseInt(arg));
 }
