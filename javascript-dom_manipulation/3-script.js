0f2530c1eeb57edc1e917ab5cfaf270d229d59bb
madi-spec49 <madi.anli.madi@gmail.com>
2026-07-02 01:33:30 +0200

    feat: add event listener to toggle header color on button click

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('toggle_header').addEventListener('click', function () {
+    const header = document.querySelector('header');
+    if (header.classList.contains('red')) {
+      header.classList.replace('red', 'green');
+    } else {
+      header.classList.replace('green', 'red');
+    }
+  });
+});
